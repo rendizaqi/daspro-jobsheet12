2bf6357b7296c0a2e3fa8d84e2bce05bf3167c23
“rendiizaqi” <rendirealme34@gmail.com>
2025-11-26 14:15:29 +0700

    menu

---
+++ Pertemuan14/Kafe18.java
@@ -0,0 +1,16 @@
+public class Kafe18 {
+    public static void menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappucino - Rp 20,000");
+        System.out.println("3. Latte - 22,000");
+        System.out.println("4. Teh Tarik - 12,000");
+        System.out.println("5. Roti Bakar - 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,0000");
+        System.out.println("========================");
+        System.out.println("Silahkan pilih menu Anda inginkan");
+    }
+    public static void main(String[] args) {
+        menu();
+    }
+}
